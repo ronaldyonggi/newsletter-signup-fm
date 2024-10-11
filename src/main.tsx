970bc54169758369da05a5ca3f44dafac3c4428e
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-10-11 09:54:55 +0800

    initial main app

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
+import './styles/reset.css';
+import './styles/fonts.css';
+import './styles/global.css';
+
 import { createRoot } from 'react-dom/client';
+import Button from './components/Button/Button';
 
 export function App() {
   return <main>
-
-  </main>
+    <Button text='Subscribe to monthly newsletter' />
+  </main>;
 }
 
 createRoot(document.getElementById('root')!).render(<App />);
